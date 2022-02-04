8ea565715abc9fad2818329de3bc907d09ee0978
antares0982@gmail.com <Antares0982@gmail.com>
2022-02-04 23:51:19 +0800

    build: export.sh print exported file name

--- export.sh
+++ export.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-if [[ -z $JAVA_HOME ]];then
+if [[ -z $JAVA_HOME ]]; then
     jpath=$(which java)
-    if [[ $? != 0 ]];then 
+    if [[ $? != 0 ]]; then 
         echo "please install java first!"
         exit
     fi
     jpath=$(readlink $jpath)
     jpath=$(dirname $(dirname $jpath))
-    if [[ -f "$HOME/.zshrc" ]];then
+    if [[ -f "$HOME/.zshrc" ]]; then
         echo "export JAVA_HOME=\"$jpath\"" >> "$HOME/.zshrc"
-        echo "JAVA_HOME is exported to .zshrc, please run:\n    source ~/.zshrc"
+        echo "JAVA_HOME is exported to ~/.zshrc, please run:\n    source ~/.zshrc"
+        exit
     else
-        if [[ -f "$HOME/.bash_profile" ]];then
-            echo "export JAVA_HOME=\"$jpath\"" >> "$HOME/.bash_profile"
-            source "$HOME/.bash_profile"
+        if [[ -f "$HOME/.bash_profile" ]]; then
+            fnm="$HOME/.bash_profile"
         else
-            if [[ -f "$HOME/.profile" ]];then
-                echo "export JAVA_HOME=\"$jpath\"" >> "$HOME/.profile"
-                source "$HOME/.profile"
+            if [[ -f "$HOME/.profile" ]]; then
+                fnm="$HOME/.profile"
             else
-                echo "export JAVA_HOME=\"$jpath\"" >> "$HOME/.bashrc"
-                source "$HOME/.bashrc"
+                fnm="$HOME/.bashrc"
             fi
         fi
     fi
@@ -30,3 +28,6 @@ else
     exit
 fi
 
+echo "export JAVA_HOME=\"$jpath\"" >> $fnm
+source $fnm
+echo "JAVA_HOME is exported to $fnm"
